89eefb66a0c1ca0bf0071ac245c528d888a65e51
Gamu2059 <yamagami0711syo@gmail.com>
2020-10-21 20:46:36 +0900

    Updates

---
+++ precache-manifest.cdd65bddce39fbedfbd25209103eff02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8048c3342157e254a81b8b07a702479d",
+    "url": "/br_system/index.html"
+  },
+  {
+    "revision": "1eee80d6302e0d5265fc",
+    "url": "/br_system/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35dfe4c8604e0d0b4d3d",
+    "url": "/br_system/static/js/2.0114d37b.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/br_system/static/js/2.0114d37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eee80d6302e0d5265fc",
+    "url": "/br_system/static/js/main.14b49a8b.chunk.js"
+  },
+  {
+    "revision": "bb5924a24b7344f85b5a",
+    "url": "/br_system/static/js/runtime-main.c2f3cd73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/br_system/static/media/logo.5d5d9eef.svg"
+  }
+]);
